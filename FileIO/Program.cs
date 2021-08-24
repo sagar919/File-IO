e2f00f1186e8371ffbf2d36c7b44c1df7be935b6
Sagar Lad <sagar.lad@morningstar.com>
2021-08-24 07:47:26 +0530

    Added File.Exists

---
+++ FileIO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+
+namespace FileIO
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"C:\Users\slad\OneDrive - MORNINGSTAR INC\Desktop\Test\git Token.txt";
+
+            if (File.Exists(path))
+                Console.WriteLine("File Exists");
+            Console.ReadKey();
+
+        }
+    }
+}
